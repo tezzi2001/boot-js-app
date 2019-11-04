35cfdca365aa3f859666c7e5f375d89df2803004
Bohdan Bondarenko <bohdan.bondarenko2001@gmail.com>
2019-11-04 22:15:34 +0200

    docs(API): add docs for class RestApiController.java

--- src/main/java/com/bondarenko/apps/boot_js_app/controlers/RestApiController.java
+++ src/main/java/com/bondarenko/apps/boot_js_app/controlers/RestApiController.java
@@ -2,37 +2,79 @@ package com.bondarenko.apps.boot_js_app.controlers;
 
 import com.bondarenko.apps.boot_js_app.entities.Note;
 import com.bondarenko.apps.boot_js_app.services.IService;
+import com.bondarenko.apps.boot_js_app.services.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+/**
+ * This is a rest controller class
+ * @author Bohdan Bondarenko
+ * @version 1.0.1
+ */
 @RestController
 public class RestApiController {
     private IService service;
 
+    /**
+     * Method for testing. Will be deleted in future versions
+     * @deprecated
+     * @return string "Main page!(test)"
+     */
+    @Deprecated
     @GetMapping("/")
     public String getDefault() {
-        return "Main page!";
+        return "Main page!(test)";
     }
 
+    /**
+     * Authorizes the user
+     * @see Service#authorize(String, String)
+     * @param request this is an input HTML form. It must contain fields "login" and "password"
+     * @return JSON object with field "isAuthorized"
+     */
     @PostMapping(value = "/login", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public boolean authorize(HttpServletRequest request) {
-        return service.authorize(request.getParameter("login"), request.getParameter("password"));
+    public Map authorize(HttpServletRequest request) {
+        boolean isAuthorized = service.authorize(request.getParameter("login"), request.getParameter("password"));
+        return new HashMap<String, Boolean>() {{
+            put("isAuthorized", isAuthorized);
+        }};
     }
 
+    /**
+     * Registers the user
+     * @see Service#register(String, String)
+     * @param request this is an input HTML form. It must contain fields "login" and "password"
+     * @return JSON object with field "isRegistered"
+     */
     @PostMapping(value = "/register", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public boolean register(HttpServletRequest request) {
-        return service.register(request.getParameter("login"), request.getParameter("password"));
+    public Map register(HttpServletRequest request) {
+        boolean isRegistered = service.register(request.getParameter("login"), request.getParameter("password"));
+        return new HashMap<String, Boolean>() {{
+            put("isRegistered", isRegistered);
+        }};
     }
 
+    /**
+     * Returns all notes from DB
+     * @see Service#getNotes()
+     * @return array of JSON objects with fields "id", "date", "login" and "record"
+     */
     @GetMapping("/getAll")
     public List<Note> getNotes() {
         return service.getNotes();
     }
 
+    /**
+     * Returns a note from DB specified by its id
+     * @see Service#getNoteById(int)
+     * @return JSON objects with fields "id", "date", "login" and "record"
+     */
     @GetMapping("/get{id}")
     public Note getNote(@PathVariable int id) {
         return service.getNoteById(id);
