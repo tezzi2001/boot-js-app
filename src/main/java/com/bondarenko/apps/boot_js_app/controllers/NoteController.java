afa76f15111ce38b579f7bd2c429890f24f946ff
Bohdan Bondarenko <bohdan.bondarenko2001@gmail.com>
2019-11-18 21:18:20 +0200

    refactor(controllers): split controller class 'RestApiController'

---
+++ src/main/java/com/bondarenko/apps/boot_js_app/controllers/NoteController.java
@@ -0,0 +1,76 @@
+package com.bondarenko.apps.boot_js_app.controllers;
+
+import com.bondarenko.apps.boot_js_app.entities.Note;
+import com.bondarenko.apps.boot_js_app.services.INoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This is a rest controller class
+ * This class delegates {@link INoteService}
+ * @author Bohdan Bondarenko
+ * @version 1.0.1
+ */
+@RestController
+public class NoteController {
+    private INoteService noteService;
+
+    @Autowired
+    public void setNoteService(INoteService noteService) {
+        this.noteService = noteService;
+    }
+
+    /**
+     * Returns all notes from DB
+     * @see INoteService#getNotes()
+     * @return array of JSON objects with fields "id", "date", "login" and "record"
+     */
+    @GetMapping("/getAll")
+    public List<Note> getNotes() {
+        return noteService.getNotes();
+    }
+
+    /**
+     * Returns a note from DB specified by its id
+     * @see INoteService#getNoteById(int)
+     * @param id An id that specifies the note
+     * @return JSON objects with fields "id", "date", "login" and "record"
+     */
+    @GetMapping("/get{id}")
+    public Note getNote(@PathVariable int id) {
+        return noteService.getNoteById(id);
+    }
+
+    /**
+     * Creates new record in DB
+     * @see INoteService#addNote(String, String, String, String)
+     * @param request this is an input HTML form. It must contain fields "login", "name", "record" and "title"
+     * @return JSON object with field "isAdded"
+     */
+    @PostMapping("/add")
+    public Map addNote(HttpServletRequest request) {
+        boolean isAdded = noteService.addNote(request.getParameter("login"), request.getParameter("name"), request.getParameter("record"), request.getParameter("title"));
+        return new HashMap<String, Boolean>() {{
+            put("isAdded", isAdded);
+        }};
+    }
+
+    /**
+     * Deletes the record in DB specified by id
+     * @see INoteService#delete(int)
+     * @param id An id that specifies the note
+     * @return JSON object with field "isDeleted"
+     */
+    @DeleteMapping("/delete{id}")
+    public Map deleteNote(@PathVariable int id) {
+        boolean isDeleted = noteService.delete(id);
+        return new HashMap<String, Boolean>() {{
+            put("isDeleted", isDeleted);
+        }};
+    }
+}
--- src/main/java/com/bondarenko/apps/boot_js_app/controllers/SignController.java
+++ src/main/java/com/bondarenko/apps/boot_js_app/controllers/SignController.java
@@ -1,36 +1,28 @@
 package com.bondarenko.apps.boot_js_app.controllers;
 
 import com.bondarenko.apps.boot_js_app.entities.Author;
-import com.bondarenko.apps.boot_js_app.entities.Note;
-import com.bondarenko.apps.boot_js_app.services.INoteService;
 import com.bondarenko.apps.boot_js_app.services.ISignService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
  * This is a rest controller class
+ * This class delegates {@link ISignService}
  * @author Bohdan Bondarenko
  * @version 1.0.1
  */
 @RestController
-public class RestApiController {
+public class SignController {
     private ISignService signService;
-    private INoteService noteService;
 
     @Autowired
     public void setSignService(ISignService signService) {
         this.signService = signService;
-    }
-
-    @Autowired
-    public void setNoteService(INoteService noteService) {
-        this.noteService = noteService;
     }
 
     /**
@@ -96,55 +88,6 @@ public class RestApiController {
         boolean isExist = signService.checkEmail(request.getParameter("email"));
         return new HashMap<String, Boolean>() {{
             put("isExist", isExist);
-        }};
-    }
-
-    /**
-     * Returns all notes from DB
-     * @see INoteService#getNotes()
-     * @return array of JSON objects with fields "id", "date", "login" and "record"
-     */
-    @GetMapping("/getAll")
-    public List<Note> getNotes() {
-        return noteService.getNotes();
-    }
-
-    /**
-     * Returns a note from DB specified by its id
-     * @see INoteService#getNoteById(int)
-     * @param id An id that specifies the note
-     * @return JSON objects with fields "id", "date", "login" and "record"
-     */
-    @GetMapping("/get{id}")
-    public Note getNote(@PathVariable int id) {
-        return noteService.getNoteById(id);
-    }
-
-    /**
-     * Creates new record in DB
-     * @see INoteService#addNote(String, String, String, String)
-     * @param request this is an input HTML form. It must contain fields "login", "name", "record" and "title"
-     * @return JSON object with field "isAdded"
-     */
-    @PostMapping(value = "/add", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public Map addNote(HttpServletRequest request) {
-        boolean isAdded = noteService.addNote(request.getParameter("login"), request.getParameter("name"), request.getParameter("record"), request.getParameter("title"));
-        return new HashMap<String, Boolean>() {{
-            put("isAdded", isAdded);
-        }};
-    }
-
-    /**
-     * Deletes the record in DB specified by id
-     * @see INoteService#delete(int)
-     * @param id An id that specifies the note
-     * @return JSON object with field "isDeleted"
-     */
-    @DeleteMapping("/delete{id}")
-    public Map deleteNote(@PathVariable int id) {
-        boolean isDeleted = noteService.delete(id);
-        return new HashMap<String, Boolean>() {{
-            put("isDeleted", isDeleted);
         }};
     }
 }
