093dc1b600b33154f1860e1afdd2afc4d8eec802
Bohdan Bondarenko <bohdan.bondarenko2001@gmail.com>
2020-01-13 03:20:27 +0200

    reafactor(controllers): add http path to CORS

--- src/main/java/com/bondarenko/apps/boot_js_app/controllers/SignController.java
+++ src/main/java/com/bondarenko/apps/boot_js_app/controllers/SignController.java
@@ -18,7 +18,7 @@ import java.util.Map;
  * @author Bohdan Bondarenko
  * @version 1.0.1
  */
-@CrossOrigin(origins = {"https://vuejs-news-app.herokuapp.com"})
+@CrossOrigin(origins = {"https://vuejs-news-app.herokuapp.com", "http://vuejs-news-app.herokuapp.com"})
 @RestController
 public class SignController {
     private ISignService service;
