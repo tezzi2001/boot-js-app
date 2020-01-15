a80c8b8bc526c8509cabb8f65d82222272eb7832
Bohdan Bondarenko <bohdan.bondarenko2001@gmail.com>
2020-01-16 00:23:24 +0200

    fix(JWTRepository): add @Transactional annotations

--- src/main/java/com/bondarenko/apps/boot_js_app/repositories/JWTRepository.java
+++ src/main/java/com/bondarenko/apps/boot_js_app/repositories/JWTRepository.java
@@ -3,8 +3,12 @@ package com.bondarenko.apps.boot_js_app.repositories;
 import com.bondarenko.apps.boot_js_app.entities.JWT;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
+
 public interface JWTRepository  extends JpaRepository<JWT, String> {
+    @Transactional
     JWT deleteJWTByRefreshToken(String refreshToken);
     boolean existsJWTByFingerprint(String fingerprint);
+    @Transactional
     void deleteJWTByFingerprint(String fingerprint);
 }
