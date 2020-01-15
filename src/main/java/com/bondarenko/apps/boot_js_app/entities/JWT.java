cf5f8c1c7cc615f857af50fa4b01c3ef3aa143b8
Bohdan Bondarenko <bohdan.bondarenko2001@gmail.com>
2020-01-15 22:04:41 +0200

    feat(JWT): add JWT session entity

---
+++ src/main/java/com/bondarenko/apps/boot_js_app/entities/JWT.java
@@ -0,0 +1,32 @@
+package com.bondarenko.apps.boot_js_app.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "tokens")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class JWT extends BaseEntity{
+    @Column(name = "login", length = 60, nullable = false)
+    private String login;
+    @Column(name = "refreshToken", length = 1300, nullable = false)
+    private String refreshToken;
+    @Column(name = "fingerprint", length = 1300, nullable = false)
+    private String fingerprint;
+    @Column(name = "expiresAt", nullable = false)
+    private Date expiresAt;
+    @Column(name = "createdAt", nullable = false)
+    private Date createdAt;
+    @Column(name = "updatedAt", nullable = false)
+    private Date updatedAt;
+}
