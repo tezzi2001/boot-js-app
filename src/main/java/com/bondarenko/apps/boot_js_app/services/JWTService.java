f4eb4c3bcf2c17fd438a7701e7bdc72ec0b9f77d
bohdan <bohdan.bondarenko2001@gmail.com>
2020-01-17 16:28:23 +0200

    chore(JWTService): correct spelling

--- src/main/java/com/bondarenko/apps/boot_js_app/services/JWTService.java
+++ src/main/java/com/bondarenko/apps/boot_js_app/services/JWTService.java
@@ -20,13 +20,13 @@ public class JWTService implements IJWTService {
     private ISignService service;
     private JWTRepository jwtRepository;
     private Algorithm algorithm;
-    private String issurer;
+    private String issuer;
 
     public JWTService(ISignService service, JWTRepository jwtRepository) {
         this.service = service;
         this.jwtRepository = jwtRepository;
         algorithm = Algorithm.HMAC256(KeyGenerators.secureRandom(50).generateKey());
-        issurer = "heroku:spring-boot-rest-api-app";
+        issuer = "heroku:spring-boot-rest-api-app";
     }
 
     @Override
@@ -103,7 +103,7 @@ public class JWTService implements IJWTService {
     public Author getAuthorFromToken(String token) {
         try {
             JWTVerifier verifier = com.auth0.jwt.JWT.require(algorithm)
-                    .withIssuer(issurer)
+                    .withIssuer(issuer)
                     .build();
             DecodedJWT jwt = verifier.verify(token);
             ObjectMapper mapper = new ObjectMapper();
@@ -120,7 +120,7 @@ public class JWTService implements IJWTService {
     private String generateRefreshToken() {
         return com.auth0.jwt.JWT
                 .create()
-                .withIssuer(issurer)
+                .withIssuer(issuer)
                 .withIssuedAt(new Date())
                 .withExpiresAt(new Date(System.currentTimeMillis()+7*60*60*1000)) // Token expires in 7 days
                 .sign(algorithm);
@@ -133,7 +133,7 @@ public class JWTService implements IJWTService {
                 .withClaim("name", author.getName())
                 .withClaim("email", author.getEmail())
                 .withClaim("role", author.getRole())
-                .withIssuer(issurer)
+                .withIssuer(issuer)
                 .withIssuedAt(new Date())
                 .withExpiresAt(new Date(System.currentTimeMillis()+20*60*1000)) // Token expires in 20 minutes
                 .sign(algorithm);
