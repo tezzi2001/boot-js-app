f3ee1fa38c2ac8fffbe7fbb3e0bdaca1c36726ec
Bohdan Bondarenko <bohdan.bondarenko2001@gmail.com>
2020-02-03 20:15:30 +0200

    refactor(JWTService): set REFRESH_TOKEN_DURATION to 1.5 minutes

--- src/main/java/com/bondarenko/apps/boot_js_app/services/JWTService.java
+++ src/main/java/com/bondarenko/apps/boot_js_app/services/JWTService.java
@@ -22,7 +22,7 @@ public class JWTService implements IJWTService {
     private String issuer;
 
     private final int ACCESS_TOKEN_DURATION = 1*60*1000; // 1 minutes
-    private final int REFRESH_TOKEN_DURATION = 7*24*60*60*1000; // Token expires in 7 days
+    private final int REFRESH_TOKEN_DURATION = 90*1000; // Token expires in 1.5 minutes
 
     public JWTService(ISignService service, JWTRepository jwtRepository) {
         this.service = service;
