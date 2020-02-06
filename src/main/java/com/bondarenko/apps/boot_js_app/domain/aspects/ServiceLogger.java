5340d1302df6181f28829f8716400a85af5f4cea
Bohdan Bondarenko <bohdan.bondarenko2001@gmail.com>
2020-02-06 22:24:27 +0200

    feat: add logger for services

---
+++ src/main/java/com/bondarenko/apps/boot_js_app/domain/aspects/ServiceLogger.java
@@ -0,0 +1,29 @@
+package com.bondarenko.apps.boot_js_app.domain.aspects;
+
+import com.bondarenko.apps.boot_js_app.repositories.JWTRepository;
+import lombok.AllArgsConstructor;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+
+import java.util.Map;
+
+@Aspect
+@AllArgsConstructor
+public class ServiceLogger {
+    private Logger logger;
+    private JWTRepository jwtRepository;
+
+    @Pointcut(value = "execution(* com.bondarenko.apps.boot_js_app.services.JWTService.refreshTokens(fingerprint))", argNames = "fingerprint")
+    private void callAtRefreshing(String fingerprint) {}
+
+    @AfterReturning(value = "callAtRefreshing(fingerprint)", returning = "map", argNames = "fingerprint,map")
+    public void log(String fingerprint, Map map) {
+        if ("OK".equals(map.get("status"))) {
+            logger.info("User " + jwtRepository.findJWTByRefreshToken(map.get("refreshToken").toString()).get().getLogin() + " with fingerprint " + fingerprint + " has refreshed JWT tokens");
+        } else {
+            logger.warn("User " + jwtRepository.findJWTByRefreshToken(map.get("refreshToken").toString()).get().getLogin() + " is not able to refresh tokens. Status: " + map.get("status"));
+        }
+    }
+}
