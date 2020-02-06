9f804ba158962f9f60bbdc95697f77957ea58271
Bohdan Bondarenko <bohdan.bondarenko2001@gmail.com>
2020-02-06 22:25:21 +0200

    refactor: delete unnecessary annotation

--- src/main/java/com/bondarenko/apps/boot_js_app/domain/annotations/AdminPermissionLog.java
+++
@@ -1,9 +0,0 @@
-package com.bondarenko.apps.boot_js_app.domain.annotations;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-@Inherited
-public @interface AdminPermissionLog {
-}
