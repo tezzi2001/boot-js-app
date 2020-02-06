34969f10a84374d565326ddf81cba4c4254cab32
Bohdan Bondarenko <bohdan.bondarenko2001@gmail.com>
2020-02-06 23:25:07 +0200

    fix: pointcut in ServiceLogger

--- src/main/java/com/bondarenko/apps/boot_js_app/domain/aspects/ServiceLogger.java
+++ src/main/java/com/bondarenko/apps/boot_js_app/domain/aspects/ServiceLogger.java
@@ -6,16 +6,18 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
+import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
 @Aspect
 @AllArgsConstructor
+@Component
 public class ServiceLogger {
     private Logger logger;
     private JWTRepository jwtRepository;
 
-    @Pointcut(value = "execution(* com.bondarenko.apps.boot_js_app.services.JWTService.refreshTokens(fingerprint))", argNames = "fingerprint")
+    @Pointcut(value = "execution(* com.bondarenko.apps.boot_js_app.services.JWTService.refreshTokens(String,..)) && args(fingerprint,..))", argNames = "fingerprint")
     private void callAtRefreshing(String fingerprint) {}
 
     @AfterReturning(value = "callAtRefreshing(fingerprint)", returning = "map", argNames = "fingerprint,map")
